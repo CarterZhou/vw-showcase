c55d9d69f560661785a5cf75780ade7e347779f7
Hao Zhou <zhou.hao.0112@gmail.com>
2013-03-22 12:30:55 +1100

    fixed bug:triggering multiple click events

--- js/fastbreak-1.0.2.js
+++ js/fastbreak-1.0.2.js
@@ -44,72 +44,76 @@ jQuery(document).ready(function($) {
         setTimeout(function(){$('div#welcome').animate({left: '0px'},800);},1000);
         // Display associated videos when one radio button is clicked.
 		$('#s_menu > li > ul input:radio').click(function(event) {
+			if($(this).hasClass('active')) {
+				$(this).removeClass('active');
                 // Slide welcome text from left to right then remove welcome div.
                 $('div#welcome').animate({left: '730px'},500,function(){$(this).remove()});
                 // Increase video container height.
                 $('div#s_video_area').css('height','920px');
                 // Show video container
                 $('div#s_video_container').css('display', 'block');
+
                 // Show subject name on top of videos
                 setTimeout($.proxy(function(){
-                	$('#s_vid_sub').empty();
-                	$('#s_video_subject').css('border-bottom','2px black solid');
-                		if($('#s_vid_img').children().length === 0){
-                        $film = $(document.createElement('img'));
-                        $film.attr({src:'http://vibewire.org/wp-content/uploads/2013/01/film_video_picture_film_roll_media.png',width:'50px',height:'50px'});
-                        $film.appendTo('#s_vid_img');
-                       }
-	                var subjectName =  $(this).val();
-	               	$subjectAnchor = $(document.createElement('a'));
-	               	$subjectAnchor
-	               	.addClass('active')
-	               	.attr('id', 's_subject_a')
-	               	.attr('href','#').appendTo('div#s_vid_sub');
-	               	$subjectAnchor.text(subjectName.toUpperCase());
-	               	$('div.s_speaker_name,div.idea_img,div.s_vid_content,div#s_review').empty();
-	               	$('<div>').addClass('loading_img').hide().appendTo($('div.s_vid_content')).fadeIn('fast');
-                },this),800);
-		        setTimeout($.proxy(function(){
-		        	// Get form data and serialize it.
-		        	$form = $('#s_form_subject');
-					var data = $form.serialize();
-			        // Fetch URLs of videos.
-		        	$.post('http://vibewire.org/fastbreak-showcase/',data , function(links, textStatus, xhr) {
-						$('div.s_vid_content').empty();
-						if($.isEmptyObject(links)===false){
-							var reviewUrl = links['review_link'] === ''? '#' : links['review_link'];
-							var urls = links['urls'];
-							var speakers = links['speakers'];
-			                document.getElementById('s_subject_a').href = reviewUrl ;
-							document.getElementById('s_subject_a').target = '_blank';
-							$('div.s_vid_content').each(function(index) {
-								if(index < urls.length){
-									$video = $('<iframe />')
-									.attr('width','360')
-									.attr('height','220')
-									.attr('src',urls[index])
-									.attr('frameborder', '0')
-									.attr('allowfullscreen', 'true').hide();
-									$ideaImg = $(document.createElement('img'));
-									$ideaImg.attr({
-										width: '30px',
-										height: '30px',
-										src:'http://vibewire.org/wp-content/uploads/2013/01/BRIGHT_IDEAS_bulb_normal.jpg'
-									}).appendTo($(this).prev().children().first());
-									$(this).prev().children().last().html("<p>"+speakers[index]+"</p>");
-									$video.appendTo(this).show();
-								}
-							});
-							var topPosition = 70 + 270 * Math.ceil(urls.length/2);
-							$bottomLink = $(document.createElement('a'))
-							.attr('href', reviewUrl)
-							.attr('target','_blank').text('Read the review here');
-							$('#s_review').append($bottomLink).css('top', topPosition);
-						}else{
-							$(document.getElementsByClassName('s_vid_content')[0]).html("<p>Sorry, no videos were found.</p>");
-						}
-					},'json');
-		   		 },this),2000);
+            	$('#s_vid_sub').empty();
+            	$('#s_video_subject').css('border-bottom','2px black solid');
+            		if($('#s_vid_img').children().length === 0){
+                    $film = $(document.createElement('img'));
+                    $film.attr({src:'http://vibewire.org/wp-content/uploads/2013/01/film_video_picture_film_roll_media.png',width:'50px',height:'50px'});
+                    $film.appendTo('#s_vid_img');
+                   }
+                var subjectName =  $(this).val();
+               	$subjectAnchor = $(document.createElement('a'));
+               	$subjectAnchor
+               	.addClass('active')
+               	.attr('id', 's_subject_a')
+               	.attr('href','#').appendTo('div#s_vid_sub');
+               	$subjectAnchor.text(subjectName.toUpperCase());
+               	$('div.s_speaker_name,div.idea_img,div.s_vid_content,div#s_review').empty();
+               	$('<div>').addClass('loading_img').hide().appendTo($('div.s_vid_content')).fadeIn('fast');
+
+	        	// Get form data and serialize it.
+	        	$form = $('#s_form_subject');
+				var data = $form.serialize();
+		        // Fetch URLs of videos.
+	        	$.post('http://vibewire.org/fastbreak-showcase/',data , function(links, textStatus, xhr) {
+					$('div.s_vid_content').empty();
+					if($.isEmptyObject(links)===false){
+						var reviewUrl = links['review_link'] === ''? '#' : links['review_link'];
+						var urls = links['urls'];
+						var speakers = links['speakers'];
+		                document.getElementById('s_subject_a').href = reviewUrl ;
+						document.getElementById('s_subject_a').target = '_blank';
+						$('div.s_vid_content').each(function(index) {
+							if(index < urls.length){
+								$video = $('<iframe />')
+								.attr('width','360')
+								.attr('height','220')
+								.attr('src',urls[index])
+								.attr('frameborder', '0')
+								.attr('allowfullscreen', 'true').hide();
+								$ideaImg = $(document.createElement('img'));
+								$ideaImg.attr({
+									width: '30px',
+									height: '30px',
+									src:'http://vibewire.org/wp-content/uploads/2013/01/BRIGHT_IDEAS_bulb_normal.jpg'
+								}).appendTo($(this).prev().children().first());
+								$(this).prev().children().last().html("<p>"+speakers[index]+"</p>");
+								$video.appendTo(this).show();
+							}
+						});
+						var topPosition = 70 + 270 * Math.ceil(urls.length/2);
+						$bottomLink = $(document.createElement('a'))
+						.attr('href', reviewUrl)
+						.attr('target','_blank').text('Read the review here');
+						$('#s_review').append($bottomLink).css('top', topPosition);
+					}else{
+						$(document.getElementsByClassName('s_vid_content')[0]).html("<p>Sorry, no videos were found.</p>");
+					}
+					$(this).addClass('active');
+				},'json');
+ 				},this),800);
+			}
 	});
 
 	$('#s_subject_a').live('mouseenter',function() {
